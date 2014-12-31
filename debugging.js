20495a23a5c4abc03878883bd58bc86a6aa3c4c4
MariellaP123 <mmp344@gmail.com>
2014-12-31 15:53:29 -0500

    renamed javascript game and put up new exercise on debugging

---
+++ debugging.js
@@ -0,0 +1,15 @@
+// Nicely written function:
+var calculate = function (number) {
+    var val = number * 10;
+    console.log(val);
+};
+
+// Badly written function with syntax errors!
+
+greeting var func{name}(console.log(name)))} 
+
+//what you want to do:
+
+
+///A big part of programming is debugging. That just means figuring out what the heck went wrong with your code. Why didn't it run?...Look at line 9. It has many syntax errors. See how lack of spacing makes debugging hard?...Fix the function on line 9. Make sure the syntax is right. Make sure it looks nice....Call the greeting function once it is fixed! Don't forget to pass in a specific name.
+
