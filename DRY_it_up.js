03c202dc01efd4fdc9c95298a1d57f88588bb5de
MariellaP123 <mmp344@gmail.com>
2014-12-31 16:39:15 -0500

    DRY it up exercise trial one

---
+++ DRY_it_up.js
@@ -0,0 +1,17 @@
+//This is a function that calculates the cost of buying 5 oranges 
+
+
+//1. You want to declare a function that calculates the cost of buying 5 oranges.
+//2. You then want to calculate the cost of the 5 all together.
+//3. Write a function that does this called orangeCost().
+//4. It should take a parameter that is the cost of an orange, and multiply it by 
+//5. It should log the result of the multiplication to the console.
+//6. Call the function where oranges each cost 5 dollars.
+
+//First draft 
+var orangeCost = function (price) {
+    total = (price *5);
+    console.log(total);
+
+
+orangeCost=5
--- debugging.js
+++ debugging.js
@@ -13,3 +13,9 @@ greeting var func{name}(console.log(name)))}
 
 ///A big part of programming is debugging. That just means figuring out what the heck went wrong with your code. Why didn't it run?...Look at line 9. It has many syntax errors. See how lack of spacing makes debugging hard?...Fix the function on line 9. Make sure the syntax is right. Make sure it looks nice....Call the greeting function once it is fixed! Don't forget to pass in a specific name.
 
+var greeting =  function(name) {
+//call the greeting function and call it a specific name 
+    console.log(name);
+};
+
+greeting();
